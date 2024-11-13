208c36abb7706925906960711d9d8ff0c051834b
Zunaira89 <161523976+Zunaira89@users.noreply.github.com>
2024-11-13 23:25:14 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import Image from "next/image";
+
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
-import Card from "./Components/Card";
-import Footer from "./Components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Hero/>
-      <Card/>
-      <Footer/>
     </div>
   );
 }
