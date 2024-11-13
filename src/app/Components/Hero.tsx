6e8e94045190a667ff515c702c26b46f2f96e6a4
Zunaira89 <161523976+Zunaira89@users.noreply.github.com>
2024-11-13 23:21:44 +0500

    first commit

---
+++ src/app/Components/Hero.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className='flex justify-right pl-20 font-bold font-serif text-4xl'>
+      <h1>
+      IMPECCABLE<br></br>
+       CRAFTSMANSHIP<br></br>
+       AND<br></br>
+       FINESSE
+       </h1>
+
+      </div>
+     <br></br>
+
+     <div className='flex justify-right pl-20 w-1/2'>
+      <p className='text-2xl text-[#787054] font-serif'>
+      An example of intricate workmanship and detail, elegant 
+       necklaces and long and short chains form a part of our
+       desirable collection.
+      </p>
+      </div>
+      <br></br>
+      <br></br>
+      
+      <div className='flex justify-right pl-20 p-15 mr-15'>
+      <button className='font-serif text-white bg-[#A29875] p-2 font-bold  text-1xl mr-15 p-15'>
+        Explore Now
+      </button>
+     </div>
+
+     <div className='flex justify-end p-5 '>
+      <img src={"/images/image.png"} alt="images" width={"400px"}
+    height={"400px"} className='  absolute top-10 right-0 mt-16 mr-5'></img>
+      </div>
+     </div>
+     )
+}
+
+export default Hero
---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='font-bold bg-[#A29875] text-6xl gap-x-4 text-white'>
+       MANZARI
+      </h1>
+    </div>
+  )
+}
+
+export default Navbar
